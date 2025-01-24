7d177debce5041189d28e90faa8b1f9b82d4c2b1
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-01-24 17:51:45 -0300

    refactor: get inventory from local storage

--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -1,98 +1,47 @@
 import { defineStore } from 'pinia'
 import IInventoryMaterial from '@/interfaces/Materials/IInventoryMaterial'
+import { useMaterialStore } from './material'
 
 export const useInventoryStore = defineStore('inventory', {
   state: () => {
-    const inventory = [
-      {
-        id: '14',
-        name: 'LF Whisperin Core',
-        rarity: 2,
-        quantity: 10,
-        family_id: '2',
-      },
-      {
-        id: '15',
-        name: 'MF Whisperin Core',
-        rarity: 3,
-        quantity: 10,
-        family_id: '2',
-      },
-      {
-        id: '16',
-        name: 'HF Whisperin Core',
-        rarity: 4,
-        quantity: 10,
-        family_id: '2',
-      },
-      {
-        id: '17',
-        name: 'FF Whisperin Core',
-        rarity: 5,
-        quantity: 10,
-        family_id: '2',
-      },
-      {
-        id: '6',
-        name: 'Inert Metallic Drip',
-        rarity: 2,
-        quantity: 1000,
-        family_id: '1',
-      },
-      {
-        id: '7',
-        name: 'Reactive Metallic Drip',
-        rarity: 3,
-        quantity: 10,
-        family_id: '1',
-      },
-      {
-        id: '8',
-        name: 'Polarized Metallic Drip',
-        rarity: 4,
-        quantity: 0,
-        family_id: '1',
-      },
-      {
-        id: '9',
-        name: 'Heterized Metallic Drip',
-        rarity: 5,
-        quantity: 0,
-        family_id: '1',
-      },
-      {
-        id: '2',
-        name: 'Crude Ring',
-        rarity: 2,
-        quantity: 100,
-        family_id: '0',
-      },
-      {
-        id: '3',
-        name: 'Basic Ring',
-        rarity: 3,
-        quantity: 10,
-        family_id: '0',
-      },
-    ] as IInventoryMaterial[];
-
     return {
-      inventory: inventory,
-      forged_inventory: [...inventory]
+      inventory: [] as IInventoryMaterial[],
+      forged_inventory: [] as IInventoryMaterial[]
     }
   },
   getters: {
-    getForgedInventory(state) {
-      return state.forged_inventory
-    },
     getInvMaterial(state) {
-      return (material_id: string) => state.inventory.find((material) => material.id === material_id)
+      return (material_id: string) => {
+        const material = state.inventory.find((material) => material.id === material_id)
+
+        if (material) {
+          return material
+        }
+
+        throw new Error(`Material with ID ${material_id} not found.`)
+      }
     },
     getForgedInvMaterial(state) {
       return (material_id: string) => state.forged_inventory.find((material) => material.id === material_id)
     },
   },
   actions: {
+    setInventory() {
+      const material_store  = useMaterialStore()
+      const inventory_local: { id: string, quantity: number }[] = JSON.parse(localStorage.getItem('inventory') || '[]')
+      const materials = material_store.materials
+
+      for (const material of materials) {
+        const material_local = inventory_local.find(material_inv_local => material_inv_local.id === material.id)
+        const inv_material: IInventoryMaterial = {
+          ...material,
+          quantity: material_local?.quantity || 0,
+        }
+
+        this.inventory.push(inv_material)
+      }
+      this.forged_inventory = JSON.parse(JSON.stringify(this.inventory))
+    },
     updateMaterialQuantity(material_id: string, new_quantity: number, forged_inv: boolean) {
       let material
 
@@ -115,27 +64,28 @@ export const useInventoryStore = defineStore('inventory', {
           material = this.forged_inventory.find(material => material.id === id)
         }
         else {
+          let inventory_local: { id: string, quantity: number }[] = JSON.parse(localStorage.getItem('inventory') || '[]')
+
           material = this.inventory.find(material => material.id === id)
+          const material_local = inventory_local.find(material => material.id === id)
+
+          if (material_local) {
+            material_local.quantity = parseInt(new_quantity)
+          }
+          else {
+            inventory_local.push({id: id, quantity: parseInt(new_quantity)})
+          }
+          localStorage.setItem('inventory', JSON.stringify(inventory_local))
+
         }
 
         if (material) {
-          material.quantity = new_quantity
+          material.quantity = parseInt(new_quantity)
         }
       }
     },
-    addMaterialQuantity(material_id: string, quantity: number, forged_inv: boolean) {
-      let material
-
-      if (forged_inv) {
-        material = this.forged_inventory.find(material => material.id === material_id)
-      }
-      else {
-        material = this.inventory.find(material => material.id === material_id)
-      }
-
-      if (material) {
-        material.quantity += quantity
-      }
+    resetForgedInventory() {
+      this.forged_inventory = JSON.parse(JSON.stringify(this.inventory))
     }
   }
 });
