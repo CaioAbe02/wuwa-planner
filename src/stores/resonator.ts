cee2df1e71a4fb177e914a7bda487233d1efcd86
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-01-24 17:53:06 -0300

    refactor: fetch resonators from supabase

--- src/stores/resonator.ts
+++ src/stores/resonator.ts
@@ -1,76 +1,45 @@
 import { defineStore } from 'pinia'
 import IResonator from '@/interfaces/Resonator/IResonator'
+import { supabase } from '@/supabase'
 
 export const useResonatorStore = defineStore('resonatorStore', {
   state: () => ({
-    resonators: [
-      {
-        id: '0',
-        name: 'Changli',
-        rarity: 5,
-        resonator_ascention_material: {
-          id: '0',
-          name: 'Rage Tacet Core',
-          rarity: 4
-        },
-        ascention_material: {
-          id: '0',
-          name: 'Pavo Plum',
-          rarity: 1
-        },
-        ascention_family_forgery_material: {
-          id: '0',
-          name: 'Ring',
-          forgery_materials: {
-            two_stars: {
-              id: '0',
-              name: 'Crude Ring',
-              rarity: 2
-            },
-            three_stars: {
-              id: '1',
-              name: 'Basic Ring',
-              rarity: 3
-            },
-            four_stars: {
-              id: '2',
-              name: 'Improved Ring',
-              rarity: 4
-            },
-            five_stars: {
-              id: '3',
-              name: 'Tailored Ring',
-              rarity: 5
-            }
-          }
-        },
-        forte_family_forgery_material: {
-          id: '1',
-          name: 'Metallic Drip',
-          forgery_materials: {
-            two_stars: {
-              id: '0',
-              name: 'Inert Metallic Drip',
-              rarity: 2
-            },
-            three_stars: {
-              id: '1',
-              name: 'Reactive Metallic Drip',
-              rarity: 3
-            },
-            four_stars: {
-              id: '2',
-              name: 'Polarized Metallic Drip',
-              rarity: 4
-            },
-            five_stars: {
-              id: '3',
-              name: 'Heterized Metallic Drip',
-              rarity: 5
-            }
-          }
-        }
-      },
-    ] as IResonator[]
+    resonators: [] as IResonator[]
   }),
+  getters: {
+    getResonator(state) {
+      return (resonator_id: string) => state.resonators.find((resonator) => resonator.id === resonator_id)
+    },
+  },
+  actions: {
+    async fetchResonators() {
+      const { data } = await supabase
+      .from('resonators')
+      .select(`
+        *,
+        resonator_ascention_material:materials!resonator_ascention_material_id("*"),
+        ascention_material:materials!ascention_material_id("*"),
+        ascention_family_forgery_material:family_material!ascention_family_forgery_material_id(
+          *,
+          two_stars:materials!two_stars_id("*"),
+          three_stars:materials!three_stars_id("*"),
+          four_stars:materials!four_stars_id("*"),
+          five_stars:materials!five_stars_id("*")
+        ),
+        forte_family_forgery_material:family_material!forte_family_forgery_material_id(
+          *,
+          two_stars:materials!two_stars_id("*"),
+          three_stars:materials!three_stars_id("*"),
+          four_stars:materials!four_stars_id("*"),
+          five_stars:materials!five_stars_id("*")
+        ),
+        skill_upgrade_material:materials!skill_upgrade_material_id("*")
+      `)
+      .returns<IResonator[]>()
+
+      if (data) {
+        this.resonators = data
+      }
+    }
+  }
 })
