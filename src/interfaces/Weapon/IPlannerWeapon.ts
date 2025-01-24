659314cd5a47d2f8ed11a91326536a8ee419d97d
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-01-24 17:50:21 -0300

    feat: IPlannerWeapon interface

---
+++ src/interfaces/Weapon/IPlannerWeapon.ts
@@ -0,0 +1,11 @@
+export default interface IPlannerWeapon {
+  id: string,
+  type: string,
+  position: number,
+  visible: boolean,
+  weapon_id: string,
+  level: number,
+  new_level: number,
+  ascention_level: number,
+  new_ascention_level: number,
+}
