1850455f7f451089c2d2de0e7163c339da6afc67
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-01-27 20:36:11 -0300

    feat: SelectResonator component

---
+++ src/components/SelectResonator.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-sheet class="pa-6">
+    <div class="d-flex flex-column ga-2">
+      <template v-for="resonator in resonators">
+        <div class="d-flex align-center ga-2 cursor-pointer" @click="selectResonator(resonator.id)">
+          <v-avatar size="large">
+            <v-img
+              :src="`src/assets/Resonators/Icons/${resonator.name}_Icon.webp`"
+            ></v-img>
+          </v-avatar>
+          <span :class="resonatorNameColor(resonator.rarity)">
+            {{ resonator.name }}
+          </span>
+        </div>
+      </template>
+    </div>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import { useResonatorStore } from '@/stores/resonator'
+
+export default defineComponent({
+  name: 'SelectResonator',
+  setup() {
+    const resonator_store = useResonatorStore()
+
+    return {
+      resonators: resonator_store.resonators
+    }
+  },
+  methods: {
+    resonatorNameColor(rarity: number) {
+      switch (rarity) {
+        case 4: return 'text-purple'
+        case 5: return 'text-yellow-lighten-2'
+      }
+    },
+    selectResonator(resonator_id: string) {
+      this.$emit('selected_resonator', resonator_id)
+    }
+  },
+})
+</script>
