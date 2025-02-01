31be865ab106d5d4220c7481a05527379303dc09
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-01-31 22:09:29 -0300

    feat: supabase utils

---
+++ src/utils/supabase.ts
@@ -0,0 +1,19 @@
+import { supabase } from "@/supabase"
+
+export function getResonatorIconUrl(resonator_name: string) {
+  const { data } = supabase.storage.from('resonators').getPublicUrl(`${resonator_name}/icon.webp`)
+
+  return data.publicUrl
+}
+
+export function getWeaponIconUrl(weapon_name: string) {
+  const { data } = supabase.storage.from('weapons').getPublicUrl(`${weapon_name.replace(' ', '_')}/icon.webp`)
+
+  return data.publicUrl
+}
+
+export function getMaterialIconUrl(material_name: string) {
+  const { data } = supabase.storage.from('materials').getPublicUrl(`${material_name.replaceAll(' ', '_')}.webp`)
+
+  return data.publicUrl
+}
