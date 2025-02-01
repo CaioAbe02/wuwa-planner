7ea79699f4a8fa620fae2691bec3035506000415
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-02-01 16:40:43 -0300

    change: root path

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,9 +9,17 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
+const customRoutes = [
+  {
+    path: '/',
+    redirect: '/planner',
+  },
+  ...routes,
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts(customRoutes),
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
