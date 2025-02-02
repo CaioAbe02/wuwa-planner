844d75293e1776c5e22bc7325c594c975bb25d3f
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-02-02 16:14:27 -0300

    feat: weapon type

---
+++ src/enums/WeaponType.ts
@@ -0,0 +1,7 @@
+export enum WeaponType {
+  Gauntlets = 'Gauntlets',
+  Broadblade = 'Broadblade',
+  Sword = 'Sword',
+  Rectifier = 'Rectifier',
+  Pistols = 'Pistols'
+}
--- src/interfaces/Weapon/IWeapon.ts
+++ src/interfaces/Weapon/IWeapon.ts
@@ -1,8 +1,10 @@
 import IFamilyForgeyMaterial from "../Materials/Forgery/IFamilyForgeryMaterial"
+import { WeaponType } from "@/enums/WeaponType"
 
 export default  interface IWeapon {
   id: string,
   name: string,
+  type: WeaponType,
   rarity: number,
   ascention_family_forgery_material: IFamilyForgeyMaterial,
   ascention_family_forgery_material_2: IFamilyForgeyMaterial,
