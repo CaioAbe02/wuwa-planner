661bf780557dabc0252ffe958ce7a048830c145d
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2025-05-17 00:52:58 -0300

    feat: filter weapons

--- src/components/SelectWeapon.vue
+++ src/components/SelectWeapon.vue
@@ -1,7 +1,49 @@
 <template>
   <v-sheet class="pa-6">
+    <v-text-field
+      label="Search"
+      variant="outlined"
+      prepend-inner-icon="mdi-magnify"
+      clearable
+      v-model="search_text"
+      @click:clear="search_text = ''"
+    ></v-text-field>
+    <div class="d-flex mb-5 ga-2">
+      <v-btn
+        icon
+        rounded="lg"
+        variant="outlined"
+        size="sm"
+        min-width="37"
+        :color="filter_rarity === 4 ? 'white' : 'grey-darken-1'"
+        @click="selectRarityFilter(4)"
+      >
+        <v-icon color="purple">mdi-rhombus</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        rounded="lg"
+        variant="outlined"
+        size="sm"
+        min-width="37"
+        :color="filter_rarity === 5 ? 'white' : 'grey-darken-1'"
+        @click="selectRarityFilter(5)">
+        <v-icon color="yellow">mdi-rhombus</v-icon>
+      </v-btn>
+      <v-btn
+        v-for="weapon_type in weapon_types"
+        icon
+        rounded="lg"
+        variant="outlined"
+        size="sm"
+        :color="filter_weapon_type === weapon_type ? 'white' : 'grey-darken-1'"
+        @click="selectWeaponTypeFilter(weapon_type)"
+      >
+        <v-img :src="`/icons/weapon_types/${weapon_type}.webp`" :aspect-ratio="1" :alt="`${weapon_type} Icon`" cover width="35" />
+      </v-btn>
+    </div>
     <div class="d-flex flex-column ga-2">
-      <template v-for="weapon in weapons">
+      <template v-for="weapon in filteredWeapons">
         <div class="d-flex align-center ga-2 cursor-pointer" @click="selectWeapon(weapon.id)">
           <v-avatar size="large">
             <v-img
@@ -22,10 +64,21 @@
 import { useWeaponStore } from '@/stores/weapon'
 
 //utils
-import { getWeaponIconUrl } from '@/utils/supabase';
+import { getWeaponIconUrl } from '@/utils/supabase'
+
+//enums
+import { WeaponType } from '@/enums/WeaponType'
 
 export default defineComponent({
   name: 'SelectWeapon',
+  data() {
+    return {
+      weapon_types: Object.values(WeaponType),
+      search_text: '',
+      filter_rarity: 0,
+      filter_weapon_type: '',
+    }
+  },
   setup() {
     const weapon_store = useWeaponStore()
 
@@ -43,7 +96,33 @@ export default defineComponent({
     },
     selectWeapon(weapon_id: string) {
       this.$emit('selected_weapon', weapon_id, false)
-    }
+    },
+    selectRarityFilter(rarity: number) {
+      if (rarity !== this.filter_rarity) {
+        this.filter_rarity = rarity
+      }
+      else {
+        this.filter_rarity = 0
+      }
+    },
+    selectWeaponTypeFilter(weapon_type: string) {
+      if (weapon_type !== this.filter_weapon_type) {
+        this.filter_weapon_type = weapon_type
+      }
+      else {
+        this.filter_weapon_type = ''
+      }
+    },
   },
+  computed: {
+    filteredWeapons() {
+      return this.weapons.filter(weapon => {
+        const matches_name = weapon.name.toLowerCase().includes(this.search_text.toLowerCase())
+        const matches_rarity = this.filter_rarity === 0 || weapon.rarity === this.filter_rarity
+        const matches_weapon_type = this.filter_weapon_type === '' || weapon.type === this.filter_weapon_type
+        return matches_name && matches_rarity && matches_weapon_type
+      })
+    }
+  }
 })
 </script>
